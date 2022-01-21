22fccffe1321ee7df3236a715ab68214bcddc9d6
Barnabe Dubus <barnabe.dubus@gmail.com>
2022-01-21 15:23:19 +0100

    moyenne

---
+++ bot.rb
@@ -0,0 +1,21 @@
+require 'net/http'
+
+def fetch(uri_str, limit = 10)
+  # You should choose a better exception.
+  raise ArgumentError, 'too many HTTP redirects' if limit == 0
+
+  response = Net::HTTP.get_response(URI(uri_str))
+
+  case response
+  when Net::HTTPSuccess then
+    response.body
+  when Net::HTTPRedirection then
+    location = response['location']
+    warn "redirected to #{location}"
+    fetch(location, limit - 1)
+  else
+    response.value
+  end
+end
+
+print fetch('https://barnabe.dev/scripts/main.js')
---
+++ moyenne.rb
@@ -0,0 +1,56 @@
+raw_notes =
+  [
+    note_a = 10.5,
+    tot_a = 16,
+  ],
+  [
+    note_b = 11,
+    tot_b = 16
+  ],
+
+  [
+    note_c = 28,
+    tot_c = 50
+
+  ],
+  [
+    note_d = 76,
+    tot_d = 100
+  ],
+  [
+    note_e = 45.25,
+    tot_e = 100
+  ]
+
+
+def note_converter(note, total)
+  result = nil
+  # note = 10.50
+  # total = 16
+  result = note.to_f / total.to_f
+  result = result * 20
+  return result
+end
+
+def array_of_notes_maker (array_of_notes)
+  converted_notes = []
+  array_of_notes.each do |notes|
+    converted_notes << note_converter(notes[0], notes[1])
+  end
+  moyenne(converted_notes)
+end
+
+def moyenne (caca)
+  puts caca.sum / caca.length
+end
+
+array_of_notes_maker(raw_notes)
+
+# note_converter(note_b, tot_b)
+# note_converter(note_c, tot_c)
+# note_converter(note_d, tot_d)
+# note_converter(note_e, tot_e)
+
+# 1ere étape : en stock les notes et les totaux dans un tableau
+# 2eme étape : on fait une boucle sur le tableau et on rempli un niuveau tanleau avec les notes sur 20
+# 3eme etape : on fait la moyenne de toute les notes sur 20
